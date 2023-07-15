82cfb8d7940d4ddac53750b279ddadade75f1c47
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-15 08:05:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function volume_sphere() {
     //Write your code here
 	const radius = +document.getElementById('radius').value;
 	const vol = 4 / 3 * Math.PI * radius ** 3;
+	// console.log(radius);
+	// console.log(vol);
 
-	document.getElementById('volume').textContent = vol;
+	document.getElementById('volume').value = vol;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
